192d0165ef8bbb601ce1a83d12d2254a66553d12
JakobJohnson <madjzj@gmail.com>
2023-11-25 11:41:06 -0800

    Completed Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const[addToCart, setIsOn] = useState(true)
+
+  function toggleCart(){
+    setIsOn((addToCart)=>!addToCart)
+  }
+
   return (
-    <li className="">
+    <li className={addToCart ? "" : "in-cart" }>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={addToCart? "add" : "remove" } onClick={toggleCart}>{addToCart ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const[filterBy, setFilterBy] = useState("All");
+  
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={(event)=>{setFilterBy(event.target.value)}}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +17,13 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {items.filter((item)=>{
+          if(filterBy === "All"){
+            return true
+          }else{
+            return item.category === filterBy;
+          }
+        }).map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
